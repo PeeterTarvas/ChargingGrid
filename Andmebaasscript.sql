a0351e9dd02749e8c3b3bb9f9350bc38da7edb17
peeter <peetertarvas@gmail.com>
2022-02-12 20:09:36 +0200

    Some minor changes to docx file

--- Andmebaasscript.sql
+++ Andmebaasscript.sql
@@ -1,10 +1,11 @@
 CREATE TABLE laadimispunktid (
      id BIGSERIAL NOT NULL,
-     laadimispunkti_kood VARCHAR(255) NOT NULL,
+     laadimispunkti_kood VARCHAR(255) NOT NULL UNIQUE,
      voimsus_voltides BIGINT NOT NULL,
      asukoht VARCHAR(255) NOT NULL,
      viimati_hooldatud TIMESTAMP,
      paeva_kasutatud BIGINT NOT NULL,
      kasutamiste_arv BIGINT NOT NULL,
+     olek
      CONSTRAINT pk_key PRIMARY KEY (id)
 )

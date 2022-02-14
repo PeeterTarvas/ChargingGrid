0f529b4ab8e81b63721fe047cfccb4222ec908db
peeter <peetertarvas@gmail.com>
2022-02-14 23:46:59 +0200

    Some minor changes to docx file

--- Andmebaasscript.sql
+++ Andmebaasscript.sql
@@ -1,14 +1,20 @@
-CREATE TABLE laadimispunktid (
-     id BIGSERIAL NOT NULL,
+
+CREATE TABLE laadimispunkt (
      laadimispunkti_kood VARCHAR(255) NOT NULL UNIQUE,
-     voimsus_voltides BIGINT NOT NULL,
+     voimsus_voltides SMALLINT NOT NULL,
      asukoht_x_koordinaat NUMERIC NOT NULL,
      asukoht_y_koordinaat NUMERIC NOT NULL,
      viimati_hooldatud TIMESTAMP,
      paeva_kasutatud BIGINT NOT NULL,
      kasutamiste_arv BIGINT NOT NULL,
-     olek VARCHAR(10) NOT NULL,
+     olek SMALLINT NOT NULL,
      registreerimise_aeg TIMESTAMP DEFAULT LOCALTIMESTAMP,
-     CONSTRAINT pk_key PRIMARY KEY (id),
-
+     CONSTRAINT pk_key PRIMARY KEY (laadimispunkti_kood),
 )
+
+ALTER TABLE laadimispunktid
+    ADD CONSTRAINT CHECK (olek BETWEEN 0 AND 2)
+
+laadimispark
+seisundil kasutada koodi nt 1, 2, 3
+moleda kuidas laadimispunkt laadimispargiga seotud on

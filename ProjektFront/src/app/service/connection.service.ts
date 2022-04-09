ebab556a4ae6f395ca0a242b9d663b7e733bea47
PeeterTarvas <peetertarvas@gmail.com>
2022-04-09 12:46:36 +0300

    add connection

---
+++ ProjektFront/src/app/service/connection.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ConnectionService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  public get(api_path: String) {
+    return this.httpClient.get('http://127.0.0.1:8000/' + api_path);
+  }
+}

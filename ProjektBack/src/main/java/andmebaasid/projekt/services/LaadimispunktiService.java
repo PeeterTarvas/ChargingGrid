20ad157aa4eb4b4c9ae87b10cb7551b9dde47aa5
Elias Markus Ehasalu <elehas@ttu.ee>
2022-04-12 15:24:51 +0300

    Fixed inactive and active api endpoints

---
+++ ProjektBack/src/main/java/andmebaasid/projekt/services/LaadimispunktiService.java
@@ -0,0 +1,50 @@
+package andmebaasid.projekt.services;
+
+import andmebaasid.projekt.entities.Laadimispunkt;
+import andmebaasid.projekt.repositories.LaadimispunktRepository;
+import andmebaasid.projekt.repositories.LaadimispunktiSeisundiLiikRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
+
+@Service
+public class LaadimispunktiService {
+
+    @Autowired
+    private LaadimispunktRepository laadimispunktRepository;
+    @Autowired
+    private LaadimispunktiSeisundiLiikRepository laadimispunktiSeisundiLiikRepository;
+
+    public List<Laadimispunkt> getALlLaadimispunkt() {
+        return laadimispunktRepository.findAll();
+    }
+
+    public Laadimispunkt getLaadimispunktWithId(Long id) {
+        return laadimispunktRepository.findByLaadimispunktiKood(id);
+    }
+
+    public List<Laadimispunkt> getAllActiveLaadimispunkt() {
+        List<Laadimispunkt> laadimispunktList = new LinkedList<>();
+        for (Laadimispunkt laadimispunkt : laadimispunktRepository.findAll()) {
+            if (Objects.equals(laadimispunktiSeisundiLiikRepository
+                    .findByLaadimispunktiSeisundiLiikKood(laadimispunkt.getLaadimispunkti_seisundi_liik_kood()).getNimetus(), "aktiivne")) {
+                laadimispunktList.add(laadimispunkt);
+            }
+        }
+        return laadimispunktList;
+    }
+
+    public List<Laadimispunkt> getAllInactiveLaadimispunkt() {
+        List<Laadimispunkt> laadimispunktList = new LinkedList<>();
+        for (Laadimispunkt laadimispunkt : laadimispunktRepository.findAll()) {
+            if (Objects.equals(laadimispunktiSeisundiLiikRepository
+                    .findByLaadimispunktiSeisundiLiikKood(laadimispunkt.getLaadimispunkti_seisundi_liik_kood()).getNimetus(), "mitteaktiivne")) {
+                laadimispunktList.add(laadimispunkt);
+            }
+        }
+        return laadimispunktList;
+    }
+}
